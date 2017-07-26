8668e0cfc3363c2e94b858876dc9d9f532cbbb54
millertronv <senjinkoha@gmail.com>
2017-07-26 10:09:00 +0100

    Euler Problem 6: Sum square difference

---
+++ euler/00006_sum_square_difference.rb
@@ -0,0 +1,14 @@
+=begin
+The sum of the squares of the first ten natural numbers is,
+1^2 + 2^2 + ... + 10^2 = 385
+
+The square of the sum of the first ten natural numbers is,
+(1 + 2 + ... + 10)^2 = 552 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+=end
+
+
+puts ((1..100).to_a.map{|x| x**2 }.inject(0, :+) - (1..100).to_a.inject(0, :+) ** 2).abs
